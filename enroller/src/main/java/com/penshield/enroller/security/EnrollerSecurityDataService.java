9ee96b0d7b2984a4a6e5494b3b29924b973590f8
Mohamed Ibrahim <msami.fawzi@gmail.com>
2016-05-27 00:35:35 +0300

    adding enroller project

---
+++ enroller/src/main/java/com/penshield/enroller/security/EnrollerSecurityDataService.java
@@ -0,0 +1,28 @@
+package com.penshield.enroller.security;
+
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.Collection;
+
+/**
+ * Created by snouto on 27/05/16.
+ */
+public class EnrollerSecurityDataService implements UserDetailsService, ApplicationContextAware {
+
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
+}
