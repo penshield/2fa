a7f835bb6b7fdba0bbbf836bba636efc5277f7ce
Mohamed Ibrahim <msami.fawzi@gmail.com>
2016-05-27 14:23:22 +0300

    adding the main Master Pages that forms the Main Dashboard of the system

--- enroller/src/main/java/com/penshield/enroller/web/security/EnrollerSecurityDataService.java
+++ enroller/src/main/java/com/penshield/enroller/web/security/EnrollerSecurityDataService.java
@@ -1,4 +1,4 @@
-package com.penshield.enroller.security;
+package com.penshield.enroller.web.security;
 
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
